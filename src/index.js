25de0c9c5cf5a7ec025ec682b2988f4c2976a8a1
haykazstepanyan <hstepanyan93@gmail.com>
2021-01-07 16:56:11 +0400

    changing icon and title, add font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
-import "./index.scss";
+import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
